3e64d708469741b99777bade1cdfe0bec3925284
ELENA <battilena@gmail.com>
2022-10-02 19:03:49 +0300

    m2

--- main_task/Program.cs
+++ main_task/Program.cs
@@ -18,3 +18,11 @@ void ArrayB(string[] array1, string[]array2){
         }
     }
 }
+void PrintArray (string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
